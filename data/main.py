327de109707b0c07739eb600e135ca1ab7d6b548
Mihhail Matvejev <m4tveevm@icloud.com>
2023-03-09 00:07:23 +0300

    New bugs!

--- data/main.py
+++ data/main.py
@@ -6,18 +6,4 @@ from data.states import main_menu
 
 
 def main():
-    # states = {
-    #     const.MAIN_MENU: main_menu.MainMenuState,
-    #     # const.LEVEL: level.LevelState,
-    #     # const.GAME_OVER: game_over.GameOverState
-    # }
-    main_menu.MainMenuState(0)
-
-    # while True:
-    #     for event in main_menu.pygame.event.get():
-    #         if event:
-    #             print(event)
-    #             main_menu.MainMenuState.get_event(event)
-    #             if main_menu.MainMenuState.get_event(event).quit:
-    #                 os.close(0)
-    #                 break
+    main_menu.MainMenuState(0).main()
--- data/states/game_screen.py
+++ data/states/game_screen.py
@@ -5,6 +5,62 @@ from data.components import tools
 
 
 class Game_screen():
+    def __init__(self):
+        self.current_time = 0.0
+        self.display = pygame.display.get_surface()
+        # self.caption = caption
+        self.keys = pygame.key.get_pressed()
+        self.states = None
+        self.state = None
+        self.running = False
+        self.clock = pygame.time.Clock()
+        self.fps = 60
+
+    def setup_states(self, states, default_state):
+        """
+        Setup states for the game
+        :param states: (dictionary): Dictionary of states
+        :param default_state: (state): Base state
+        """
+        self.states = states
+        self.state = states[default_state](self.current_time, None)
+
+    def flip_state(self):
+        props = self.state.cleanup()
+        self.state = self.states[self.state.next](self.current_time, props)
+
+    def update(self):
+        """
+        Updates the current state
+        """
+
+        self.current_time = pygame.time.get_ticks()
+        if self.state.quit:
+            self.running = False
+        elif self.state.done:
+            self.flip_state()
+        self.state.update(self.display, self.current_time)
+
+    def event_loop(self):
+        """
+        Event processing loop
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            self.state.get_event(event)
+
+    def main(self):
+        """
+        Main loop of the game
+        """
+        self.running = True
+        while self.running:
+            self.event_loop()
+            self.update()
+            pygame.display.update()
+            self.clock.tick(self.fps)
+
     def screen_init(self):
         pygame.init()
         self.screen = pygame.display.set_mode(const.SCREEN_SIZE)
