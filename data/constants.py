6ab6e9d327d097720da647b5b1912aa9fd759dd9
Mihhail Matvejev <m4tveevm@icloud.com>
2023-03-11 00:41:41 +0300

    Updated version v0.1 Beta

--- data/constants.py
+++ data/constants.py
@@ -1,13 +1,10 @@
-from data.states.main_menu import MainMenuState
+GAME_VERSION = '0.1v'
 
-GAME_VERSION = '0.1'
-
-GAME_NAME = 'CyberBug 2084™'
+GAME_NAME = f'CyberBug 2084™ - Beta {GAME_VERSION}'
 MAIN_MENU = 'menu'
 SETTINGS = 'settings'
 LEVEL = 'level'
 BUG = 'bug'
-
 
 STATUS = [MAIN_MENU, SETTINGS, LEVEL, BUG]
 
@@ -18,5 +15,4 @@ BG_COLOR = 'yellow'
 SCREEN_CENTER = SCREEN_SIZE[0] // 2
 
 GAME_ICON_LOCATION = 'favicon.jpeg'
-FPS = 60
 THE_BEST_GAME_EVER = 'BrawlStars'
--- data/main.py
+++ data/main.py
@@ -1,11 +1,10 @@
 from data import constants as const
-# from data import setup
 from data.states import main_menu, level, actual_gameplay
 
 states = {
     const.MAIN_MENU: main_menu.MainMenuState,
     const.LEVEL: level.LevelState,
-    const.BUG: actual_gameplay.Bug_Screen
+    const.BUG: actual_gameplay.Bug_screen
 }
 
 
